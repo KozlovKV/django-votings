b81c4d498c1f608eed251a29f1c0e013f7bdd284
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2020-12-19 17:50:46 +0600

    Set settings in sv_app

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from sv_app.views import test_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', test_page),
 ]
