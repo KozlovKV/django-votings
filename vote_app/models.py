c764505aac94b1deca7e89e58a7d52cbbaf8e10e
Kibenko Daria <stegankaeasy@gmail.com>
2020-12-26 12:36:28 +0300

    Class Votes

--- vote_app/models.py
+++ vote_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Votes(models.Model):
+    User_id = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    Votings_id = models.ForeignKey(to=Votings, on_delete=models.CASCADE)
+    Choices_id = models.ForeignKey(to=Choices, on_delete=models.CASCADE)
+    Date_vote = models.DateTimeField(auto_now_add=True)
+
