866a10989e826489f8bdea87d38296d9d7a125d7
Pavel Domkin <paveldomkin@gmail.com>
2020-12-26 12:44:43 +0000

    List_of_votes

--- vote_app/models.py
+++ vote_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from vote_app.models import Votings
 
-# Create your models here.
+
+class Vote_Variants(models.Model):
+    ID_voting = models.ForeignKey(to=Votings, on_delete=models.CASCADE)
+    Serial_number = models.IntegerField()
+    Description = models.TextField()
+    Counts_of_votes = models.IntegerField()
