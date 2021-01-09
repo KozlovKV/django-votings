162406269a58ef3e967cc4250f9918cb5debe12c
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2021-01-09 15:52:00 +0600

    Added logic of writing additional info during registration

--- profile_app/view_subclasses.py
+++ profile_app/view_subclasses.py
@@ -7,6 +7,8 @@ from menu_app.view_subclasses import TemplateViewWithMenu
 
 import profile_app.forms as profile_forms
 
+import profile_app.models as profile_models
+
 
 class LoginViewDetailed(auth_views.LoginView, TemplateViewWithMenu):
     form_class = profile_forms.ModifiedAuthenticationForm
@@ -15,6 +17,12 @@ class LoginViewDetailed(auth_views.LoginView, TemplateViewWithMenu):
 class RegistrationViewDetailed(reg_act_views.RegistrationView, TemplateViewWithMenu):
     form_class = profile_forms.ModifiedRegistrationForm
     success_url = reverse_lazy('django_registration_complete')
+
+    def register(self, form):
+        new_user = super(RegistrationViewDetailed, self).register(form)
+        additional_info = profile_models.AdditionUserInfo(User_id=new_user)
+        additional_info.save()
+        return new_user
 
 
 class ActivationViewDetailed(reg_act_views.ActivationView, TemplateViewWithMenu):
