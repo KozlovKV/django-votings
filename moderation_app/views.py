1612b3f21fa056cd0182e007ccf7b9cfaa35f845
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2021-01-16 01:06:59 +0600

    Added view-class context for moder main page

--- moderation_app/views.py
+++ moderation_app/views.py
@@ -1,4 +1,5 @@
 from menu_app.view_subclasses import TemplateViewWithMenu
+from moderation_app.models import Reports
 
 
 class TestModerView(TemplateViewWithMenu):
@@ -7,6 +8,24 @@ class TestModerView(TemplateViewWithMenu):
 
 class ModerationPanelView(TemplateViewWithMenu):
     template_name = 'moderation_main.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ModerationPanelView, self).get_context_data(**kwargs)
+        context.update({
+            'reports': {
+                'all': len(Reports.objects.all()),
+                'submitted': len(Reports.objects.filter(status=1)),
+                'rejected': len(Reports.objects.filter(status=2)),
+                'processed': len(Reports.objects.exclude(status=0)),
+            },
+            'change_request': {
+                'all': len(Reports.objects.all()),
+                'submitted': len(Reports.objects.filter(status=1)),
+                'rejected': len(Reports.objects.filter(status=2)),
+                'processed': len(Reports.objects.exclude(status=0)),
+            },
+        })
+        return context
 
 
 class ChangeRequestsListView(TemplateViewWithMenu):
@@ -25,7 +44,8 @@ class ReportsListView(TemplateViewWithMenu):
     template_name = 'reports_list.html'
 
 
-class SendReportView(TemplateViewWithMenu):  # TODO: https://docs.djangoproject.com/en/3.1/ref/class-based-views/generic-editing/
+class SendReportView(
+    TemplateViewWithMenu):  # TODO: https://docs.djangoproject.com/en/3.1/ref/class-based-views/generic-editing/
     template_name = 'send.html'
 
     def get(self, request, *args, **kwargs):
@@ -35,4 +55,3 @@ class SendReportView(TemplateViewWithMenu):  # TODO: https://docs.djangoproject.
             Да в общем всё что душе угодно (При ненадобности можно вообще удалить)
         """
         return super(SendReportView, self).get(self, request, *args, **kwargs)
-
