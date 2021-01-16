58d32cc1253bfa1974a3d315c94e56c0d16851d7
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2021-01-17 01:05:23 +0600

    Renamed some model fields to snake_case, added logic of adding and deleting of btns

---
+++ vote_app/static/vote_app/js/variants_script.js
@@ -0,0 +1,43 @@
+let add_btn = document.getElementById('add_vote_variant')
+
+let variants_container = document.getElementById('vote_variants')
+
+let variant_blocks = variants_container.getElementsByClassName('list-elem')
+let varinats_count = document.getElementById('variants_count')
+let saved_variant_block = variant_blocks[0]
+
+function delete_variant_block(event) {
+    event.target.parentNode.remove()
+    update_variants_context()
+}
+
+function create_variant_block(serial_number) {
+    block = saved_variant_block.cloneNode(true)
+    let input = block.getElementsByTagName('input')[0]
+    input.name = 'variant_' + String(serial_number)
+    input.placeholder = 'Вариант ' + String(serial_number + 1)
+    block.getElementsByTagName('button')[0].addEventListener('click', delete_variant_block)
+    return block
+}
+
+function add_new_variant(event) {
+    serial_number = variant_blocks.length
+    new_variant_block = create_variant_block(serial_number)
+    variants_container.appendChild(new_variant_block)
+    update_variants_context()
+}
+
+function update_variants_context() {
+    varinats_count.value = variant_blocks.length
+    for (let i = 0; i < variant_blocks.length; ++i) {
+        block = variant_blocks[i]
+        block.getElementsByTagName('button')[0].addEventListener('click', delete_variant_block)
+        let input = block.getElementsByTagName('input')[0]
+        input.name = 'variant_' + String(i)
+        input.placeholder = 'Вариант ' + String(i + 1)
+    }
+}
+
+add_btn.addEventListener('click', add_new_variant)
+
+for (block of variant_blocks) { block.getElementsByTagName('button')[0].addEventListener('click', delete_variant_block) }
--- vote_app/views.py
+++ vote_app/views.py
@@ -4,7 +4,7 @@ import django.views.generic.edit as generic_edit
 
 from menu_app.view_menu_context import get_full_menu_context
 from menu_app.view_subclasses import TemplateViewWithMenu
-from vote_app.forms import VoteConfigForm, ModeledVoteCreateForm, ModeledVoteEditForm
+from vote_app.forms import ModeledVoteCreateForm, ModeledVoteEditForm
 
 
 from vote_app.models import Votings
@@ -25,6 +25,7 @@ class CreateVotingView(TemplateViewWithMenu, generic_edit.CreateView):
     object = None
     model = Votings
     form_class = ModeledVoteCreateForm
+    success_url = reverse_lazy('vote_list')
 
     def get_context_data(self, **kwargs):
         context = super(CreateVotingView, self).get_context_data(**kwargs)
@@ -38,10 +39,12 @@ class CreateVotingView(TemplateViewWithMenu, generic_edit.CreateView):
         post_response = super(CreateVotingView, self).post(self, request, *args, **kwargs)
 
         # TODO: Добавить сохранение вариантов голосования
+        variants_list = get_variants_list(self.request)
+        print(variants_list)
 
         # Записать ID новго голосования для переадресации
-        voting_id = self.object.id
-        self.success_url = reverse_lazy('vote_view', args=(voting_id,))
+        # voting_id = self.object.id
+        # post_response.url = reverse_lazy('vote_view', args=(voting_id,))
         return post_response
 
 
@@ -56,7 +59,7 @@ class EditVotingView(TemplateViewWithMenu, generic_edit.FormView):
         context = super(EditVotingView, self).get_context_data(**kwargs)
         context.update({
             'voting_id': kwargs["voting_id"],
-            'context_url': reverse('vote_create', args=(kwargs["voting_id"],)),
+            'context_url': reverse('vote_edit', args=(kwargs["voting_id"],)),
         })
         return context
 
@@ -66,9 +69,16 @@ class EditVotingView(TemplateViewWithMenu, generic_edit.FormView):
         # TODO: Добавить сохранение вариантов голосования и создание записи в модели запросов на редактирование
 
         # Записать ID новго голосования для переадресации
-        voting_id = self.object.id
-        self.success_url = reverse_lazy('vote_view', args=(voting_id,))
+        # voting_id = self.object.id
+        # post_response.url = reverse_lazy('vote_view', args=(voting_id,))
         return post_response
+
+
+def get_variants_list(request):
+    res = []
+    for serial_num in range(0, int(request.POST.get('variants_count'))):
+        res.append(request.POST.get(f'variant_{serial_num}'))
+    return res
 
 
 def get_variants_context(voting_id):
@@ -79,8 +89,8 @@ def get_variants_context(voting_id):
         variant_dict = {
             'serial_number': variant.Serial_number,
             'description': variant.Description,
-            'votes_count': variant.Counts_of_votes,
-            'percent': (variant.Counts_of_votes*100)/voting.Votes,
+            'votes_count': variant.Votes_count,
+            'percent': (variant.Votes_count * 100) / voting.Vote_variants,
         }
         res.append(variant_dict)
     res.sort(key=lambda x: x['serial_number'])
@@ -100,12 +110,12 @@ class VotingView(TemplateViewWithMenu, generic_edit.FormView):
             'title': voting_note.Title,
             'description': voting_note.Description,
             'author': voting_note.Author,
-            'status': voting_note.ComplaintState,
+            'status': voting_note.Complaint_state,
             'image': voting_note.Image,
-            'resultSeeWho': voting_note.ResultSeeWho,
-            'resultSeeWhen': voting_note.ResultSeeWhen,
-            'votes_count': voting_note.Votes,
-            'end_date': voting_note.EndDate,
+            'resultSeeWho': voting_note.Result_see_who,
+            'resultSeeWhen': voting_note.Result_see_when,
+            'votes_count': voting_note.Vote_variants,
+            'end_date': voting_note.End_date,
             'vote_variants': get_variants_context(voting_id)
         })
         return context
