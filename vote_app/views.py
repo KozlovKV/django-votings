24a6fc6f6af94f3483910e0b939309bba7b8756c
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2021-01-16 22:54:58 +0600

    VoteView micro-edit

--- vote_app/views.py
+++ vote_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 import django.views.generic.edit as generic_edit
 
 from menu_app.view_menu_context import get_full_menu_context
@@ -20,36 +20,11 @@ class VoteListPageView(TemplateViewWithMenu):
     template_name = 'vote_list.html'
 
 
-# TODO: САМЫЙ ОПТИМАЛЬНЫЙ ВАРИАНТ - https://docs.djangoproject.com/en/3.1/ref/class-based-views/generic-editing/
-# def vote_create_page(request):
-#     context = {'voting_id': -1}
-#     vote = VoteConfigForm()
-#     context.update(get_full_menu_context(request))
-#     context.update({'form': vote})
-#     if request.POST:
-#         record = Votings(
-#             Title=request.POST.get('title'),
-#             Image=request.POST.get('image'),
-#             Description=request.POST.get('description'),
-#             Author=request.POST.get('user'),
-#             ComplaintState=0,
-#             ResultSeeWho=request.POST.get('see_who'),
-#             ResultSeeWhen=request.POST.get('see_when'),
-#             AnonsCanVote=(request.POST.get('anons_can') == 'True'),
-#             Type=request.POST.get('type'),
-#             Votes=0
-#         )
-#         record.save()
-#     # context.update({'history': Votings.objects.all()})
-#     return render(request, 'vote_config.html', context)
-
-
 class CreateVotingView(TemplateViewWithMenu, generic_edit.CreateView):
     template_name = 'vote_config.html'
     object = None
     model = Votings
-    form_class = ModeledVoteEditForm
-    success_url = '/vote/list/'
+    form_class = ModeledVoteCreateForm
 
     def get_context_data(self, **kwargs):
         context = super(CreateVotingView, self).get_context_data(**kwargs)
@@ -60,8 +35,14 @@ class CreateVotingView(TemplateViewWithMenu, generic_edit.CreateView):
         return context
 
     def post(self, request, *args, **kwargs):
+        post_response = super(CreateVotingView, self).post(self, request, *args, **kwargs)
+
         # TODO: Добавить сохранение вариантов голосования
-        return super(CreateVotingView, self).post(self, request, *args, **kwargs)
+
+        # Записать ID новго голосования для переадресации
+        voting_id = self.object.id
+        self.success_url = reverse_lazy('vote_view', args=(voting_id,))
+        return post_response
 
 
 class EditVotingView(TemplateViewWithMenu, generic_edit.FormView):
@@ -69,7 +50,7 @@ class EditVotingView(TemplateViewWithMenu, generic_edit.FormView):
     object = None  # TODO: принимать существующую запись
     model = Votings
     form_class = ModeledVoteEditForm
-    success_url = '/vote/list/'
+    success_url = reverse_lazy('vote_list')
 
     def get_context_data(self, **kwargs):
         context = super(EditVotingView, self).get_context_data(**kwargs)
@@ -80,47 +61,52 @@ class EditVotingView(TemplateViewWithMenu, generic_edit.FormView):
         return context
 
     def post(self, request, *args, **kwargs):
+        post_response = super(EditVotingView, self).post(self, request, *args, **kwargs)
+
         # TODO: Добавить сохранение вариантов голосования и создание записи в модели запросов на редактирование
-        return super(EditVotingView, self).post(self, request, *args, **kwargs)
+
+        # Записать ID новго голосования для переадресации
+        voting_id = self.object.id
+        self.success_url = reverse_lazy('vote_view', args=(voting_id,))
+        return post_response
 
 
-def get_variants_context(voting_id, request):
+def get_variants_context(voting_id):
     res = []
     vote_variants = VoteVariants.objects.filter(ID_voting=voting_id)
     voting = Votings.get(pk=voting_id)
     for variant in vote_variants:
-        variant_dict = {'serial_number': variant.Serial_number,
-                        'description': variant.Description,
-                        'votes_count': variant.Counts_of_votes,
-                        'percent': (variant.Counts_of_votes*100)/voting.Votes,
-                        }
+        variant_dict = {
+            'serial_number': variant.Serial_number,
+            'description': variant.Description,
+            'votes_count': variant.Counts_of_votes,
+            'percent': (variant.Counts_of_votes*100)/voting.Votes,
+        }
         res.append(variant_dict)
     res.sort(key=lambda x: x['serial_number'])
     return res
 
+
 class VotingView(TemplateViewWithMenu, generic_edit.FormView):
-    template_name = 'vote_config.html'
-    object = None  # TODO: принимать существующую запись
-    model = Votings
-    form_class = ModeledVoteEditForm
-    success_url = '/vote/list/'
+    template_name = 'vote_test.html'
+
     def get_context_data(self, **kwargs):
         context = super(VotingView, self).get_context_data(**kwargs)
         voting_id = kwargs["voting_id"]
         voting_note = Votings.objects.get(pk=voting_id)
+        self.success_url = reverse_lazy('vote_view', args=(voting_id,))
         context.update({
             'voting_id': kwargs["voting_id"],
-            'context_url': reverse('vote_create', args=(kwargs["voting_id"],)),
-            'vote_header' : voting_note.Title,
-            'Description': voting_note.Description,
-            'Author': voting_note.Author,
-            'Status': voting_note.ComplaintState,
-            'Image' : voting_note.Image,
-            'ResultSeeWho': voting_note.ResultSeeWho,
-            'ResultSeeWhen': voting_note.ResultSeeWhen,
-            'Vote_counts': voting_note.Votes,
-            'End_date' : voting_note.EndDate,
-            'vote_variants' : get_variants_context(voting_id, self.request)
+            'title': voting_note.Title,
+            'description': voting_note.Description,
+            'author': voting_note.Author,
+            'status': voting_note.ComplaintState,
+            'image': voting_note.Image,
+            'resultSeeWho': voting_note.ResultSeeWho,
+            'resultSeeWhen': voting_note.ResultSeeWhen,
+            'votes_count': voting_note.Votes,
+            'end_date': voting_note.EndDate,
+            'vote_variants': get_variants_context(voting_id)
         })
         return context
 
