12ce87dff6b492d94a7938303ca94f923fa36836
Kibenko Daria <stegankaeasy@gmail.com>
2021-01-17 14:49:53 +0300

    VotingEditRequestForm

--- moderation_app/forms.py
+++ moderation_app/forms.py
@@ -8,3 +8,14 @@ class CommentForm(forms.Form):
             'placeholder': 'Напишите комментарий',
             'class': 'input wide',
         }))
+
+class EditRequestForm(forms.Form):
+    description = forms.CharField(label = 'Комментарий к решению', widget = forms.Textarea(attrs={
+        'placeholder': 'Комментарий к решению',
+        'class' : 'input',
+    }))
+    reset_votes = forms.BooleanField(required=False, label='Обнулить ли голоса', widget = forms.NullBooleanSelect(attrs={
+        'class': 'input'
+    }))
+
+    
