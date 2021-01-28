436ae7f120f5b5aa13e647f5757b621e4f9940f2
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2021-01-28 20:19:26 +0600

    Make common meta for voting create and edit forms

--- vote_app/forms.py
+++ vote_app/forms.py
@@ -3,50 +3,55 @@ from django import forms
 from vote_app.models import Votings
 
 
+class VoteConfigMeta:
+    model = Votings
+    fields = [
+        'title', 'description', 'image',
+        'type', 'anons_can_vote',
+        'result_see_who', 'result_see_when',
+        'end_date', 'variants_count', 'author',
+    ]
+    labels = {
+        'title': 'Заголовок',
+        'image': 'Картинка',
+        'description': 'Описание',
+        'type': 'Тип голосования',
+        'anons_can_vote': 'Разрешить голосовать анонимам',
+        'result_see_who': 'Кому видны результаты',
+        'result_see_when': 'Когда видные результаты',
+        'end_date': 'Дата окончания (пусто - бессрочно)',
+    }
+    widgets = {
+        'title': forms.TextInput(attrs={
+            'class': 'input',
+        }),
+        'image': forms.FileInput(attrs={
+            'class': 'input',
+        }),
+        'description': forms.Textarea(attrs={
+            'class': 'input ultra-wide',
+        }),
+        'type': forms.RadioSelect,
+        'result_see_who': forms.RadioSelect,
+        'result_see_when': forms.RadioSelect,
+        'end_date': forms.DateTimeInput(attrs={
+            'type': 'datetime-local',
+            'placeholder': 'YYYY-MM-DD HH:MM[:SS]',
+        }),
+        'variants_count': forms.NumberInput(attrs={
+            'type': 'hidden',
+            'id': 'variants_count',
+        }),
+        'author': forms.TextInput(attrs={
+            'type': 'hidden',
+            'id': 'author',
+        }),
+    }
+
+
 class ModeledVoteCreateForm(forms.ModelForm):
-    class Meta:
-        model = Votings
-        fields = [
-            'title', 'description', 'image',
-            'type', 'anons_can_vote',
-            'result_see_who', 'result_see_when',
-            'end_date', 'variants_count', 'author',
-        ]
-        labels = {
-            'title': 'Заголовок',
-            'image': 'Картинка',
-            'description': 'Описание',
-            'type': 'Тип голосования',
-            'anons_can_vote': 'Разрешить голосовать анонимам',
-            'result_see_who': 'Кому видны результаты',
-            'result_see_when': 'Когда видные результаты',
-            'end_date': 'Дата окончания (пусто - бессрочно)',
-        }
-        widgets = {
-            'title': forms.TextInput(attrs={
-                'class': 'input',
-            }),
-            'image': forms.FileInput(attrs={
-                'class': 'input',
-            }),
-            'description': forms.Textarea(attrs={
-                'class': 'input ultra-wide',
-            }),
-            'type': forms.RadioSelect,
-            'result_see_who': forms.RadioSelect,
-            'result_see_when': forms.RadioSelect,
-            'end_date': forms.DateTimeInput(attrs={
-                'type': 'datetime-local',
-            }),
-            'variants_count': forms.NumberInput(attrs={
-                'type': 'hidden',
-                'id': 'variants_count',
-            }),
-            'author': forms.TextInput(attrs={
-                'type': 'hidden',
-                'id': 'author',
-            }),
-        }
+    class Meta(VoteConfigMeta):
+        pass
 
 
 class ModeledVoteEditForm(forms.ModelForm):
@@ -55,42 +60,5 @@ class ModeledVoteEditForm(forms.ModelForm):
                                     'class': 'input wide',
                               }))
 
-    class Meta:
-        model = Votings
-        fields = [
-            'title', 'image', 'description',
-            'type', 'anons_can_vote',
-            'result_see_who', 'result_see_when',
-            'end_date', 'variants_count',
-        ]
-        labels = {
-            'title': 'Заголовок',
-            'image': 'Картинка',
-            'description': 'Описание',
-            'type': 'Тип голосования **',
-            'anons_can_vote': 'Разрешить голосовать анонимам **',
-            'result_see_who': 'Кому видны результаты *',
-            'result_see_when': 'Когда видные результаты *',
-            'end_date': 'Дата окончания (пусто - бессрочно) *',
-        }
-        widgets = {
-            'title': forms.TextInput(attrs={
-                'class': 'input',
-            }),
-            'image': forms.FileInput(attrs={
-                'class': 'input',
-            }),
-            'description': forms.Textarea(attrs={
-                'class': 'input ultra-wide',
-            }),
-            'type': forms.RadioSelect(),
-            'result_see_who': forms.RadioSelect,
-            'result_see_when': forms.RadioSelect,
-            'end_date': forms.DateTimeInput(attrs={
-                'type': 'datetime-local',
-            }),
-            'variants_count': forms.NumberInput(attrs={
-                'type': 'hidden',
-                'id': 'variants_count',
-            }),
-        }
+    class Meta(VoteConfigMeta):
+        pass
